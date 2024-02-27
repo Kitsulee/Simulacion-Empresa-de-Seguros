328584f330bb3739ed9bdef76198b54f40b255ba
Diana Laura Pérez Trujillo <108586028+Kitsulee@users.noreply.github.com>
2024-02-27 01:20:33 -0500

    agregadas etiquetas para graficos comparativos

--- test_lambda.py
+++ test_lambda.py
@@ -15,6 +15,7 @@ m= 0.1 # Tasa de retención
 weib_a= 5 # Parámetro de forma de la distribución de Weibull
 T= 100 # Duración de la simulación
 std_threshold= 5 # Umbral de desviación estándar
+labels = ['lamb = 25', 'lamb = 23', 'lamb = 22', 'lamb = 20']
 
 #Ejecución de la simulación
 
@@ -24,7 +25,7 @@ execute(std_threshold, n_0, a_0, lamb3, c, v, m, weib_a, T, "Lambda", 22)
 execute(std_threshold, n_0, a_0, lamb4, c, v, m, weib_a, T, "Lambda", 20)
 
 #Comparar los diferentes casos prueba
-compare()
+compare(labels)
 
 execute_dis(std_threshold, n_0, a_0, lamb1, c, v, m, weib_a, T, "Lambda", 25)
 execute_dis(std_threshold, n_0, a_0, lamb2, c, v, m, weib_a, T, "Lambda", 23)
